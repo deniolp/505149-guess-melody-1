4a28e369ed43e72063562292bb79898207797e4d
popov7 <deniolp@gmail.com>
2019-05-01 10:08:32 +0300

    Implementing getting of variables from index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,16 @@ import ReactDom from 'react-dom';
 import App from './components/app/app.jsx';
 
 const init = () => {
+  const settings = {
+    gameTime: 5,
+    errorCount: 3,
+  };
+
   ReactDom.render(
-      <App />,
+      <App
+        gameTime={settings.gameTime}
+        errorCount={settings.errorCount}
+      />,
       document.querySelector(`.main`)
   );
 };
