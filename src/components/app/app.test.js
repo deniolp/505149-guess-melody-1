6621a08e2f2b6732ba1a0145fdb4926740cf2675
popov7 <deniolp@gmail.com>
2019-05-04 17:42:45 +0300

    Adding 'describe' to snapshot tests

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -2,12 +2,14 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import App from '../app/app.jsx';
 
-it(`App renders correctly`, () => {
-  const tree = renderer.create(<App
-    gameTime={0}
-    errorCount={0}
-    onClick = {jest.fn()}
-  />).toJSON();
+describe(`App`, () => {
+  it(`renders correctly`, () => {
+    const tree = renderer.create(<App
+      gameTime={0}
+      errorCount={0}
+      onClick = {jest.fn()}
+    />).toJSON();
 
-  expect(tree).toMatchSnapshot();
+    expect(tree).toMatchSnapshot();
+  });
 });
