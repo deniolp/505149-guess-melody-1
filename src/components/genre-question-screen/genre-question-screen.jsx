2f3ad5429b82730c6c595b5221e86e381219e0f3
popov7 <deniolp@gmail.com>
2019-05-07 15:46:54 +0300

    Creating GenreQuestionScreen component

---
+++ src/components/genre-question-screen/genre-question-screen.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const GenreQuestionScreen = (props) => {
+  const {answers, genre} = props;
+
+  return <section className="game__screen">
+    <h2 className="game__title">Выберите {genre} треки</h2>
+    <form className="game__tracks">
+      {
+        answers.map((item, index) => <div className="track" key={`answer-${index}`}>
+          <button className="track__button track__button--play" type="button"></button>
+          <div className="track__status">
+            <audio src={item.src}></audio>
+          </div>
+          <div className="game__answer">
+            <input className="game__input visually-hidden" type="checkbox" name="answer" value={`answer-${index}`} id={`answer-${index}`}/>
+            <label className="game__check" htmlFor={`answer-${index}`}>Отметить</label>
+          </div>
+        </div>)
+      }
+      <button className="game__submit button" type="submit">Ответить</button>
+    </form>
+  </section>;
+};
+
+export default GenreQuestionScreen;
