b2be1c0619b767af257c41004e5741671504d135
popov7 <deniolp@gmail.com>
2019-05-07 20:55:04 +0300

    Updating snapshot tests and snapshots

--- src/components/genre-question-screen/genre-question-screen.test.js
+++ src/components/genre-question-screen/genre-question-screen.test.js
@@ -3,33 +3,36 @@ import renderer from 'react-test-renderer';
 import GenreQuestionScreen from '../genre-question-screen/genre-question-screen.jsx';
 
 const mock = {
-  genre: `blues`,
-  answers: [
-    {
-      src: ``,
-      genre: `blues`,
-    },
-    {
-      src: ``,
-      genre: `rock`,
-    },
-    {
-      src: ``,
-      genre: `jazz`,
-    },
-    {
-      src: ``,
-      genre: `blues`,
-    }
-  ]
+  question: {
+    type: `genre`,
+    genre: `blues`,
+    answers: [
+      {
+        src: ``,
+        genre: `blues`,
+      },
+      {
+        src: ``,
+        genre: `rock`,
+      },
+      {
+        src: ``,
+        genre: `jazz`,
+      },
+      {
+        src: ``,
+        genre: `blues`,
+      }
+    ]
+  }
 };
 
 describe(`GenreQuestionScreen`, () => {
   it(`renders correctly`, () => {
-    const {answers, genre} = mock;
+    const {question} = mock;
     const tree = renderer.create(<GenreQuestionScreen
-      answers={answers}
-      genre={genre}
+      question={question}
+      onAnswer={jest.fn()}
     />).toJSON();
 
     expect(tree).toMatchSnapshot();
