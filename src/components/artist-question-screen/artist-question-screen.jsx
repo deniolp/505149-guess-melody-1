c67aa7b061df81ca7080f612621d8fe7c87430f0
popov7 <deniolp@gmail.com>
2019-05-07 19:29:26 +0300

    Correcting Proptypes in artist-question-screen

--- src/components/artist-question-screen/artist-question-screen.jsx
+++ src/components/artist-question-screen/artist-question-screen.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const ArtistQuestionScreen = (props) => {
-  const {answers, song} = props;
+const ArtistQuestionScreen = (question, onAnswer) => {
+  const {answers, song} = question;
 
   return <section className="game__screen">
     <h2 className="game__title">Кто исполняет эту песню?</h2>
@@ -11,7 +11,7 @@ const ArtistQuestionScreen = (props) => {
       <audio src={song.src}></audio>
     </div>
 
-    <form className="game__artist">
+    <form className="game__artist" onChange={onAnswer}>
       {
         answers.map((item, index) => <div className="artist" key={index}>
           <input className="artist__input visually-hidden" type="radio" name="answer" value={`artist-${index}`} id={`artist-${index}`}/>
@@ -26,14 +26,19 @@ const ArtistQuestionScreen = (props) => {
 };
 
 ArtistQuestionScreen.propTypes = {
-  answers: PropTypes.arrayOf(PropTypes.shape({
-    picture: PropTypes.string.isRequired,
-    artist: PropTypes.string.isRequired,
-  })),
-  song: PropTypes.shape({
-    artist: PropTypes.string.isRequired,
-    src: PropTypes.string.isRequired,
-  })
+  onAnswer: PropTypes.func.isRequired,
+  question: PropTypes.shape({
+    answers: PropTypes.arrayOf(PropTypes.shape({
+      picture: PropTypes.string.isRequired,
+      artist: PropTypes.string.isRequired,
+    })),
+    song: PropTypes.shape({
+      artist: PropTypes.string.isRequired,
+      src: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+  type: PropTypes.oneOf([`genre`, `artist`]),
 };
+
 
 export default ArtistQuestionScreen;
