7ec9ea26068bb39d5771a883ba8f65fb15fe9ff3
popov7 <deniolp@gmail.com>
2019-05-07 14:48:59 +0300

    Creating mock file with questions and answers

---
+++ src/mocks/questions.js
@@ -0,0 +1,45 @@
+export default [
+  {
+    type: `genre`,
+    genre: `blues`,
+    answers: [
+      {
+        src: `https://upload.wikimedia.org/wikipedia/commons/f/f4/Irish_national_anthem%2C_performed_by_the_United_States_Navy_Band.wav`,
+        genre: `rock`,
+      },
+      {
+        src: `https://upload.wikimedia.org/wikipedia/commons/f/f4/Irish_national_anthem%2C_performed_by_the_United_States_Navy_Band.wav`,
+        genre: `jazz`,
+      },
+      {
+        src: `https://upload.wikimedia.org/wikipedia/commons/f/f4/Irish_national_anthem%2C_performed_by_the_United_States_Navy_Band.wav`,
+        genre: `blues`,
+      },
+      {
+        src: `https://upload.wikimedia.org/wikipedia/commons/f/f4/Irish_national_anthem%2C_performed_by_the_United_States_Navy_Band.wav`,
+        genre: `blues`,
+      }
+    ]
+  },
+  {
+    type: `artist`,
+    song: {
+      artist: `John Lennon`,
+      src: `https://upload.wikimedia.org/wikipedia/commons/f/f4/Irish_national_anthem%2C_performed_by_the_United_States_Navy_Band.wav`
+    },
+    answers: [
+      {
+        picture: `http://placehold.it/134x134`,
+        artist: `Kurt Cobain`,
+      },
+      {
+        picture: `http://placehold.it/134x134`,
+        artist: `John Lennon`,
+      },
+      {
+        picture: `http://placehold.it/134x134`,
+        artist: `Zemfira`,
+      }
+    ],
+  },
+];
