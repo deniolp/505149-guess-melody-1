0df745474a62b4574855275712d86982f6da7228
popov7 <deniolp@gmail.com>
2019-05-07 17:31:34 +0300

    Making App a class that extends Component

--- src/index.js
+++ src/index.js
@@ -4,14 +4,11 @@ import App from './components/app/app.jsx';
 import settings from './mocks/settings';
 import questions from './mocks/questions';
 
-const startGame = () => {};
-
 const init = (gameQuestions) => {
   ReactDom.render(
       <App
         gameTime={settings.GAMETIME}
         errorCount={settings.ERRORCOUNT}
-        onClick={startGame}
         questions={gameQuestions}
       />,
       document.querySelector(`.main`)
