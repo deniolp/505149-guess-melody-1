33c4899f76117325d324933047bfcf3c4759137e
popov7 <deniolp@gmail.com>
2019-05-14 21:46:05 +0300

    Writing e2e test for AudioPlayer component

---
+++ src/components/audio-player/audio-player.e2e.test.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import Enzyme, {mount} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import AudioPlayer from '../audio-player/audio-player';
+
+Enzyme.configure({adapter: new Adapter()});
+
+const mock = {
+  src: `https://upload.wikimedia.org/wikipedia/commons/f/f4/Irish_national_anthem%2C_performed_by_the_United_States_Navy_Band.wav`,
+};
+
+let clickHandler;
+let audioPlayer;
+let playButton;
+
+beforeEach(() => {
+  clickHandler = jest.fn();
+  audioPlayer = mount(
+      <AudioPlayer
+        src={mock.src}
+        isPlaying={false}
+        onPlayButtonClick={clickHandler}
+      />);
+  playButton = audioPlayer.find(`.track__button`);
+});
+
+it(`First click on button makes isPlaying true`, () => {
+  audioPlayer.setState({isLoading: false});
+
+  playButton.simulate(`click`, {preventDefault() {}});
+  audioPlayer.update();
+
+  expect(audioPlayer.state(`isPlaying`)).toEqual(true);
+  playButton.simulate(`click`, {preventDefault() {}});
+  audioPlayer.update();
+  expect(audioPlayer.state(`isPlaying`)).toEqual(false);
+});
+
+it(`Next click on button makes isPlaying false`, () => {
+  playButton.simulate(`click`, {preventDefault() {}});
+  audioPlayer.update();
+  expect(audioPlayer.state(`isPlaying`)).toEqual(false);
+});
