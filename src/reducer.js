e9f4ffda9351f3a2077ec4e8352cb381e5810c89
popov7 <deniolp@gmail.com>
2019-05-17 12:01:45 +0300

    Adding reducer to reducer.js

--- src/reducer.js
+++ src/reducer.js
@@ -2,3 +2,19 @@ const initialState = {
   step: -1,
   mistakes: 0,
 };
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case `INCREMENT_STEP`: return Object.assign({}, state, {
+      step: state.step + action.payload,
+    });
+
+    case `INCREMENT_MISTAKES`: return Object.assign({}, state, {
+      mistakes: state.mistakes + action.payload,
+    });
+
+    case `RESET`: return Object.assign({}, initialState);
+  }
+
+  return state;
+};
