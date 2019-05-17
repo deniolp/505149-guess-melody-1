7a5150d0873a45c89c488121d226ae019055f4b2
popov7 <deniolp@gmail.com>
2019-05-17 13:00:01 +0300

    Writing others tests for reducer

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -7,4 +7,40 @@ describe(`Reducer works correctly: `, () => {
       mistakes: 0,
     });
   });
+
+  it(`should increment current step by given number`, () => {
+    expect(reducer({
+      step: 0,
+      mistakes: 0,
+    }, {
+      type: `INCREMENT_STEP`,
+      payload: 1,
+    })).toEqual({
+      step: 1,
+      mistakes: 0,
+    });
+  });
+  it(`should increment current mistake by given number`, () => {
+    expect(reducer({
+      step: 0,
+      mistakes: 0,
+    }, {
+      type: `INCREMENT_MISTAKES`,
+      payload: 1,
+    })).toEqual({
+      step: 0,
+      mistakes: 1,
+    });
+  });
+  it(`should reset state correctly`, () => {
+    expect(reducer({
+      step: 7687346,
+      mistakes: 628,
+    }, {
+      type: `RESET`,
+    })).toEqual({
+      step: -1,
+      mistakes: 0,
+    });
+  });
 });
