181385d5d1d39e1a33422c38e3b237d38a7d9f3e
popov7 <deniolp@gmail.com>
2019-05-19 14:34:00 +0300

    Adding REDUX_DEVTOOLS to project

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import settings from './mocks/settings';
 import questions from './mocks/questions';
 import {reducer} from './reducer';
 
-const store = createStore(reducer);
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 const init = (gameQuestions) => {
   ReactDom.render(<Provider store={store}>
