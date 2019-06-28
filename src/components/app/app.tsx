3f39b7c35b4a73071e279b380390d2c78b2061e6
popov7 <deniolp@gmail.com>
2019-06-28 11:55:12 +0300

    Creating general Types and preparing App component for TS

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,26 +1,27 @@
-import {Component} from 'react';
-import PropTypes from 'prop-types';
+import * as React from 'react';
 import {connect} from 'react-redux';
 
 import withScreenSwitch from '../../hocs/with-screen-switch/with-screen-switch';
 import {getStep} from '../../reducer/game/selectors';
 import {getQuestions} from '../../reducer/data/selectors';
+import {QuestionArtist, QuestionGenre} from "../../types";
 
-class App extends Component {
+type Question = QuestionArtist | QuestionGenre;
+interface Props {
+  gameTime: number,
+  onClick: () => void,
+  renderScreen: (question: Question) => React.ReactElement,
+  questions: Question[],
+  step: number,
+}
+
+class App extends React.PureComponent<Props, null> {
   render() {
     const {questions, step, renderScreen} = this.props;
 
     return renderScreen(questions[step]);
   }
 }
-
-App.propTypes = {
-  gameTime: PropTypes.number.isRequired,
-  onClick: PropTypes.func,
-  renderScreen: PropTypes.func.isRequired,
-  questions: PropTypes.array.isRequired,
-  step: PropTypes.number.isRequired,
-};
 
 const mapStateToProps = (state) => ({
   step: getStep(state),
